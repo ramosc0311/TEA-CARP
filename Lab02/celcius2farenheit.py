6b37b249bf9fc9127e6e15043136c4f46a9f5ded
carlos.ramos <carlos.ramos@est.zamorano.edu>
2022-09-11 16:59:13 -0600

    lab02

---
+++ Lab02/celcius2farenheit.py
@@ -0,0 +1,7 @@
+# Tendencias e Innovacion en Tecnologia Agricola
+
+temperaturaCelcius = float(input("¿Cual es la temperatura en Celcius?"))
+temperaturaFahrenheit = (9/5) * temperaturaCelcius + 32
+print(temperaturaFahrenheit)
+
+print(type(temperaturaFahrenheit))
---
+++ Lab02/pagoporhora.py
@@ -0,0 +1,10 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+#   Pago por Hora
+horas = input("¿horas trabajadas?")
+valorPorHora = input("¿valor por hora?")
+
+total = int(horas) * int(valorPorHora)
+
+print(total)
+
+print(type(total))
